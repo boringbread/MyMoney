30864b02ca2ccd050c28d3f712e4fa964d88d727
astro-rizki <srizkiafrizal85@gmail.com>
2018-07-20 00:26:58 +0700

    done : - bottom bar - top bar (on going)

--- app/build/generated/source/r/debug/android/support/mediacompat/R.java
+++ app/build/generated/source/r/debug/android/support/mediacompat/R.java
@@ -58,18 +58,18 @@ public final class R {
         public static final int subtitle_shadow_radius = 0x7f0600d5;
     }
     public static final class drawable {
-        public static final int notification_action_background = 0x7f070077;
-        public static final int notification_bg = 0x7f070078;
-        public static final int notification_bg_low = 0x7f070079;
-        public static final int notification_bg_low_normal = 0x7f07007a;
-        public static final int notification_bg_low_pressed = 0x7f07007b;
-        public static final int notification_bg_normal = 0x7f07007c;
-        public static final int notification_bg_normal_pressed = 0x7f07007d;
-        public static final int notification_icon_background = 0x7f07007e;
-        public static final int notification_template_icon_bg = 0x7f07007f;
-        public static final int notification_template_icon_low_bg = 0x7f070080;
-        public static final int notification_tile_bg = 0x7f070081;
-        public static final int notify_panel_notification_icon_bg = 0x7f070082;
+        public static final int notification_action_background = 0x7f07007a;
+        public static final int notification_bg = 0x7f07007b;
+        public static final int notification_bg_low = 0x7f07007c;
+        public static final int notification_bg_low_normal = 0x7f07007d;
+        public static final int notification_bg_low_pressed = 0x7f07007e;
+        public static final int notification_bg_normal = 0x7f07007f;
+        public static final int notification_bg_normal_pressed = 0x7f070080;
+        public static final int notification_icon_background = 0x7f070081;
+        public static final int notification_template_icon_bg = 0x7f070082;
+        public static final int notification_template_icon_low_bg = 0x7f070083;
+        public static final int notification_tile_bg = 0x7f070084;
+        public static final int notify_panel_notification_icon_bg = 0x7f070085;
     }
     public static final class id {
         public static final int action0 = 0x7f080006;
@@ -91,20 +91,20 @@ public final class R {
         public static final int line1 = 0x7f080062;
         public static final int line3 = 0x7f080063;
         public static final int media_actions = 0x7f080067;
-        public static final int normal = 0x7f080076;
-        public static final int notification_background = 0x7f080077;
-        public static final int notification_main_column = 0x7f080078;
-        public static final int notification_main_column_container = 0x7f080079;
-        public static final int right_icon = 0x7f080086;
-        public static final int right_side = 0x7f080087;
-        public static final int status_bar_latest_event_content = 0x7f0800af;
-        public static final int tag_transition_group = 0x7f0800b4;
-        public static final int tag_unhandled_key_event_manager = 0x7f0800b5;
-        public static final int tag_unhandled_key_listeners = 0x7f0800b6;
-        public static final int text = 0x7f0800b7;
-        public static final int text2 = 0x7f0800b8;
-        public static final int time = 0x7f0800c1;
-        public static final int title = 0x7f0800c2;
+        public static final int normal = 0x7f080077;
+        public static final int notification_background = 0x7f080078;
+        public static final int notification_main_column = 0x7f080079;
+        public static final int notification_main_column_container = 0x7f08007a;
+        public static final int right_icon = 0x7f080087;
+        public static final int right_side = 0x7f080088;
+        public static final int status_bar_latest_event_content = 0x7f0800b0;
+        public static final int tag_transition_group = 0x7f0800b5;
+        public static final int tag_unhandled_key_event_manager = 0x7f0800b6;
+        public static final int tag_unhandled_key_listeners = 0x7f0800b7;
+        public static final int text = 0x7f0800b8;
+        public static final int text2 = 0x7f0800b9;
+        public static final int time = 0x7f0800c4;
+        public static final int title = 0x7f0800c5;
     }
     public static final class integer {
         public static final int cancel_button_image_alpha = 0x7f090004;
